dc9efd0aba309ce5bf007378bd0ebf140a54fb07
khianvictorycalderon <ckhianvictory@gmail.com>
2026-01-06 10:27:09 +0800

    Added to do list views and template

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {
+        "todos": items
+    })
